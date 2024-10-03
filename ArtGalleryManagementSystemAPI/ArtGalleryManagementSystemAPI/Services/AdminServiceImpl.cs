ee130ebda1d4ae97e4372ed0ac31414ca7017c87
thomaplam007 <thongst07vn@gmail.com>
2024-10-03 17:32:00 +0700

    Thong Update delete/add User

--- ArtGalleryManagementSystemAPI/ArtGalleryManagementSystemAPI/Services/AdminServiceImpl.cs
+++ ArtGalleryManagementSystemAPI/ArtGalleryManagementSystemAPI/Services/AdminServiceImpl.cs
@@ -14,6 +14,44 @@ public class AdminServiceImpl : AdminService
         mapper = _mapper;
     }
 
+    public bool CreateUserSeller(UserDto userdto)
+    {
+        try
+        {
+            var user = mapper.Map<User>(userdto);
+            db.Users.Add(user);
+
+            if (db.SaveChanges() > 0)
+            {
+                if (user.Role == 2)
+                {
+                    var seller = new Seller();
+                    seller.Id = user.Id;
+                    seller.Income = 0;
+                    seller.CreatedAt = user.CreatedAt;
+                    db.Sellers.Add(seller);
+                }
+                var cart = new Cart();
+                cart.Id = user.Id;
+                cart.Total = 0;
+                cart.CreatedAt = user.CreatedAt;
+                db.Carts.Add(cart);
+            }
+            return db.SaveChanges() > 0;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public bool Delete(UserDto userDto)
+    {
+        var user = mapper.Map<User>(userDto);
+        db.Entry(user).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+        return db.SaveChanges() > 0;
+    }
+
     public List<UserDto> FindAllSeller()
     {
         return mapper.Map<List<UserDto>>(db.Users.Where(u => u.Role == 2).ToList());
